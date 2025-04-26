b715aa5edd154a8c0553048682431204fd41094e
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-26 14:41:01 -0300

    feat: add FireSwordItem that sets targets on fire after hitting

---
+++ src/main/java/com/felipemdutra/moreStuff/item/custom/FireSwordItem.java
@@ -0,0 +1,21 @@
+package com.felipemdutra.moreStuff.item.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
+
+public class FireSwordItem extends CustomSwordItem {
+    private static final float fireDurationSecs = 5.0f;
+
+    public FireSwordItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        super.postHit(stack, target, attacker);
+
+        // apply fire effect for 6 seconds
+        target.setOnFireFor(fireDurationSecs);
+    }
+}
