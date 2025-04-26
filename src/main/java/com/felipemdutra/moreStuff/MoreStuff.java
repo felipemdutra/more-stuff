b0c5eabad44cbe88651779fec531495e1debb6e2
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-26 16:41:27 -0300

    initialize CustomToolMaterials class on initialize

--- src/main/java/com/felipemdutra/moreStuff/MoreStuff.java
+++ src/main/java/com/felipemdutra/moreStuff/MoreStuff.java
@@ -1,6 +1,7 @@
 package com.felipemdutra.moreStuff;
 
 import com.felipemdutra.moreStuff.item.ModItems;
+import com.felipemdutra.moreStuff.item.custom.CustomToolMaterials;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class MoreStuff implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Initializing {}", ID);
 
+        CustomToolMaterials.initialize();
         ModItems.initialize();
     }
 }
