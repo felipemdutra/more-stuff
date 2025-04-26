f952330f37be156455690a7bdc6ca64b2743f90f
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-26 00:42:08 -0300

    feat: call 'ModItems.initialize()' on initialize

--- src/main/java/com/felipemdutra/moreStuff/MoreStuff.java
+++ src/main/java/com/felipemdutra/moreStuff/MoreStuff.java
@@ -1,5 +1,6 @@
 package com.felipemdutra.moreStuff;
 
+import com.felipemdutra.moreStuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,5 +11,8 @@ public class MoreStuff implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Initializing {}", ID);
+
+        ModItems.initialize();
     }
 }
