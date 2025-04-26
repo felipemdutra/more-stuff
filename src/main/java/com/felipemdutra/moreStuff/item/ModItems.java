c639ce04c7762921ff0faff2f0fa7baf42aa8df8
Felipe Dutra <felipemd.dutra@gmail.com>
2025-04-26 00:29:33 -0300

    implement initial version of ModItems Added public static 'register' methods that register items to Minecraft's items Registry Added private helper 'keyOf' to abstract registry keys Added 'initialize' method that just logs a debug message for now

--- src/main/java/com/felipemdutra/moreStuff/item/ModItems.java
+++ src/main/java/com/felipemdutra/moreStuff/item/ModItems.java
@@ -1,4 +1,32 @@
 package com.felipemdutra.moreStuff.item;
 
+import com.felipemdutra.moreStuff.MoreStuff;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
 public class ModItems {
+    @SuppressWarnings("unchecked")
+    private static <T extends Item> RegistryKey<T> keyOf(String id) {
+        return (RegistryKey<T>) RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MoreStuff.ID, id));
+    }
+
+    public static <T extends Item> T register(String id, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        return register(keyOf(id), factory, settings);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Item> T register(RegistryKey<Item> key, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        T item = (T) factory.apply(settings.registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
+    }
+
+    public static void initialize() {
+        MoreStuff.LOGGER.info("ModItems initialized!");
+    }
 }
